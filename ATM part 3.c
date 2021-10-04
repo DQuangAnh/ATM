77a30d77552a2a16d6d1651cda746a6125f86354
Đinh Quang Anh <91303593+DQuangAnh@users.noreply.github.com>
2021-10-04 12:26:31 +0700

    Update ATM part 3.c

--- ATM part 3.c
+++ ATM part 3.c
@@ -18,56 +18,55 @@ main()
 			printf("\nLogged in successfully. Please choose a transaction.");
 			printf("\n 1.Withdrow money.\n 2.Tranfer money.\n 3.Checking account balance.\n 4.End transaction.");
 			printf("\nChoose your transaction: ");
-	        scanf("%d", &choice);
+	                scanf("%d", &choice);
 	 	   	switch(choice)
-	       {
-	         case 1:
-	            printf("\nYour choice is withdraw money.");
-	            printf("\nPlease enter the amount you want to withdraw. ");
-	            scanf("%f", &withdrawamount);
+	                 {
+	                   case 1:
+	                     printf("\nYour choice is withdraw money.");
+	                     printf("\nPlease enter the amount you want to withdraw. ");
+	                     scanf("%f", &withdrawamount);
 	            
-	            if(withdrawamount <= balance)
-	             {
-	             	balance = balance - withdrawamount;
-	             	printf("\nWithdraw money successfully.\nYour account balance is %f", balance);
-	             }
-	            else 
-				    printf("\nYour account balance is not enough."); 
-	             	
-		     break;
-	         case 2:
-	            printf("\nYour choice is transfer money.");
-	            printf("\nPlease enter the account you want to tranfer:");
-	            scanf("%d", &tranfer1);
-	            if(tranfer1 == 5678)
-	            {
-	            	printf("\nPlease enter the amount you want to tranfer:");
-	            	scanf("%f", &tranferamount);
-	            	if(tranferamount <= balance)
-	            	{
-	            		balance = balance - tranferamount;
-	            		printf("\nTranfer successfully.\nYour account balance is %f", balance);
-	            	}
+	                     if(withdrawamount <= balance)
+	                      {
+	             	            balance = balance - withdrawamount;
+	             	            printf("\nWithdraw money successfully.\nYour account balance is %f", balance);
+	                      }
+	                     else 
+				    printf("\nYour account balance is not enough.");
+		             break;
+	                   case 2:
+	                     printf("\nYour choice is transfer money.");
+	                     printf("\nPlease enter the account you want to tranfer:");
+	                     scanf("%d", &tranfer1);
+	                     if(tranfer1 == 5678)
+	                      {
+	            	            printf("\nPlease enter the amount you want to tranfer:");
+	            	            scanf("%f", &tranferamount);
+	            	         if(tranferamount <= balance)
+	            	         {
+	            		    balance = balance - tranferamount;
+	            		    printf("\nTranfer successfully.\nYour account balance is %f", balance);
+	            	          }
 					else
 					    printf("\nYour balance is not enough.");
 				}
-				else
+			     else
 				    printf("\nThe account you want to tranfer incorrect.Please try again.");			
-	         break;
-	         case 3:
-	            printf("\nYour choice is checking account balance.");
-	            printf("\nYour account balance is %f", &balance);
-		     break;
-	         case 4:
-	            printf("\nYour choice is end transaction.");
-	            printf("\nSee you again.");
-		     break;
-	            default:
-	            printf("\nInvalid choice!! re-selected please.");
-		    }
-		  }
-	 	else
-		   printf("\nPassword incorrect! Please try again.");
+	                     break;
+	                     case 3:
+	                       printf("\nYour choice is checking account balance.");
+	                       printf("\nYour account balance is %f", &balance);
+		               break;
+	                     case 4:
+	                       printf("\nYour choice is end transaction.");
+	                       printf("\nSee you again.");
+		               break;
+	                     default:
+	                       printf("\nInvalid choice!! re-selected please.");
+		      }
+		     }
+	 	      else
+		         printf("\nPassword incorrect! Please try again.");
 		   }
 	else
 	    printf("Username incorrect! Please try again.");
