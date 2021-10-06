79ff43a3a7d9b37aee3c1221f14693570d78f396
Đinh Quang Anh <91303593+DQuangAnh@users.noreply.github.com>
2021-10-06 12:35:54 +0700

    Add files via upload

--- ATM part 4.c
+++ ATM part 4.c
@@ -1,10 +1,12 @@
 // ATM part 4
 #include<stdio.h>
+#include<stdlib.h>
 main()
 {
 	int username, password, choice, tranfer1;
 	double balance = 100000000.00;
 	double withdrawamount, tranferamount;
+	char choose;
 	
 	printf("Enter your username: ");
 	scanf("%d", &username);
@@ -15,9 +17,9 @@ main()
 	{
 		printf("\nUsername incorrect!");
 		printf("\nEnter your username: ");
-  	        scanf("%d", &username);
-  	        printf("\nEnter your password: ");
-  	        scanf("%d", &password);   
+  	    scanf("%d", &username);
+  	    printf("\nEnter your password: ");
+  	    scanf("%d", &password);   
   	}
   	
   	while(password != 1999)
@@ -25,74 +27,80 @@ main()
   		printf("\nPassword incorrect!");
   		printf("\nEnter your username: ");
   		scanf("%d", &username);
-                printf("\nEnter your password: ");
-	        scanf("%d", &password);
+        printf("\nEnter your password: ");
+	    scanf("%d", &password);
 	}
 	
 	if(username == 1234 && password == 1999)
 	 {
-  
-			printf("\nLogged in successfully. Please choose a transaction.");
+			printf("\nLogged in successfully.");		
+		do
+		{
+			printf("\nPlease choose a transaction.");
 			printf("\n 1.Withdrow money.\n 2.Tranfer money.\n 3.Checking account balance.\n 4.End transaction.");
 			printf("\nChoose your transaction: ");
-	                scanf("%d", &choice);
-	         
-		        while(choice != 1 && choice !=2 && choice !=3 && choice !=4)
-		        {
-		    	    printf("\nInvalid choice!");
-		    	    printf("\nChoose your transaction: ");
-	                    scanf("%d", &choice);	
+	        scanf("%d", &choice);
+	        
+		    while(choice != 1 && choice !=2 && choice !=3 && choice !=4)
+		    {
+		    	printf("\nInvalid choice!");
+		    	printf("\nChoose your transaction: ");
+	            scanf("%d", &choice);	
 		   	}
 	 	   	
 			switch(choice)
-	                {
-	                 case 1:
-	                    printf("\nYour choice is withdraw money.");
-	                    printf("\nPlease enter the amount you want to withdraw. ");
-	                    scanf("%lf", &withdrawamount);
-	                    if(withdrawamount <= balance)
-	                    {
-	             	      balance = balance - withdrawamount;
-	             	      printf("\nWithdraw money successfully.");
-			      printf("\nYour account balance is %lf", balance);
-	                     }
-	                    else 
-			      printf("\nYour account balance is not enough."); 	
-		         break;
-	                 case 2:
-	                   printf("\nYour choice is transfer money.");
-	                   printf("\nPlease enter the account you want to tranfer:");
-	                   scanf("%d", &tranfer1);
+	       {
+	         case 1:
+	            printf("\nYour choice is withdraw money.");
+	            printf("\nPlease enter the amount you want to withdraw. ");
+	            scanf("%lf", &withdrawamount);
+	            if(withdrawamount <= balance)
+	             {
+	             	balance = balance - withdrawamount;
+	             	printf("\nWithdraw money successfully.");
+					printf("\nYour account balance is %lf", balance);
+	             }
+	            else 
+				    printf("\nYour account balance is not enough."); 	
+		     break;
+	         case 2:
+	            printf("\nYour choice is transfer money.");
+	            printf("\nPlease enter the account you want to tranfer:");
+	            scanf("%d", &tranfer1);
 	            
-	                   while(tranfer1 != 5678)
-	                   {
-	            	     printf("The account you want to tranfer is not correct! Please try again.");
-	            	     printf("\nPlease enter the account you want to tranfer:");
-	                     scanf("%d", &tranfer1);
-	                   }    
+	            while(tranfer1 != 5678)
+	            {
+	            	printf("The account you want to tranfer is not correct! Please try again.");
+	            	printf("\nPlease enter the account you want to tranfer:");
+	                scanf("%d", &tranfer1);
+	            }    
 	            
-			  if(tranfer1 == 5678)
-	                  {
-	            	     printf("\nPlease enter the amount you want to tranfer:");
-	            	     scanf("%lf", &tranferamount);
-	            	     if(tranferamount <= balance)
-	            	      {
+				if(tranfer1 == 5678)
+	            {
+	            	printf("\nPlease enter the amount you want to tranfer:");
+	            	scanf("%lf", &tranferamount);
+	            	if(tranferamount <= balance)
+	            	{
 	            		balance = balance - tranferamount;
 	            		printf("\nTranfer successfully.\nYour account balance is %lf", balance);
-	            	      }
-		          else
-		             printf("\nYour balance is not enough.");
-			  }			
-	                break;
-	                case 3:
-	                   printf("\nYour choice is checking account balance.");
-	                   printf("\nYour account balance is %lf", balance);
-		        break;
-	                case 4:
-	                   printf("\nYour choice is end transaction.");
-	                   printf("\nSee you again.");
-		        break;
-		        }
-	 }
-	return 0;
+	            	}
+					else
+					    printf("\nYour balance is not enough.");
+				}			
+	         break;
+	         case 3:
+	            printf("\nYour choice is checking account balance.");
+	            printf("\nYour account balance is %lf", balance);
+		     break;
+	         case 4:
+	            printf("\nYour choice is end transaction.");
+		     break;
+		    };
+		    printf("\n Do you want to continue an other transaction?");
+		    printf("\nEnter 'Y' or 'y' if you agree, enter 'N' or 'n' if you dont: ");
+		    scanf("%c", &choose);
+		}
+		while(choose == 'y' || choose == 'Y');
+	}
+       return 0;
 }		 	 	      
